e768b87f684d03b8ed479d23aa78e0e0e770523a
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-15 21:17:56 +0200

    Modified launcher

--- src/main/java/ua/lviv/iot/launcher/Launcher.java
+++ src/main/java/ua/lviv/iot/launcher/Launcher.java
@@ -1,18 +1,27 @@
 package main.java.ua.lviv.iot.launcher;
 
+import java.util.Scanner;
+
 import main.java.ua.lviv.iot.algorithm.NewtonAlgorithmForTwoArguments;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		double[] input = {1.0,1.0};
-		NewtonAlgorithmForTwoArguments algo = new NewtonAlgorithmForTwoArguments(input);
+		double[] primaryArgumentsGuess = new double[2];
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter first argument primary guess: ");
+		primaryArgumentsGuess[0] = input.nextDouble();
+		System.out.println("Enter second argument primary guess: ");
+		primaryArgumentsGuess[1] = input.nextDouble();
+		input.close();
+		NewtonAlgorithmForTwoArguments algo = new NewtonAlgorithmForTwoArguments(primaryArgumentsGuess);
 		double[] result = algo.findRoots();
-		for(double x:result) {
+		for (double x : result) {
 			System.out.println(x);
 		}
 		double[] functionValues = algo.getFunctionsVector(result);
-		for(double value:functionValues) {
+		System.out.println("\nFunctions' values:\n");
+		for (double value : functionValues) {
 			System.out.println(value);
 		}
 	}
